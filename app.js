a94042765557320616cdbb3f3a2bbdf9e4658f0c
FedeSeg <fedu.segura@gmail.com>
2021-05-05 21:53:00 -0300

    agrego htmls

--- app.js
+++ app.js
@@ -10,10 +10,23 @@ app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
-app.get('/articles', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/articles.html'));
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
 });
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
