9fb55e63592b774cff98a88cfdcd5435a42d0fb9
FedeSeg <fedu.segura@gmail.com>
2021-05-26 19:12:43 -0300

    nuevos cambios para routeo

--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 //Deberia borrar el /productDetail de router.get porque ya esta especificado en app.js
+router.get('/productDetail/:idProducto', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
 
+
+});
+
+module.exports = router
+
+/* 
 router.get('/productDetail', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
 });
 
-module.exports = router
+req.params.idProducto ------> para tomar el numero de id
+
+*/
