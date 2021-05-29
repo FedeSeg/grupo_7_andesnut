c3645eab67d480ab141d3a4446cc852f0ad89356
FedeSeg <fedu.segura@gmail.com>
2021-05-28 23:33:26 -0300

    controladores y ejs3 falta dominio variable

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const publicPath = path.resolve(__dirname, '../public')
 
 app.set('view engine', 'ejs');
 
-app.set( 'views' , './views' )
+app.set( 'views' , './src/views' )
 
 app.use( express.static(publicPath) )
 
