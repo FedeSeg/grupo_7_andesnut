1bc33e76283b35f01f72eff397af1f411886d540
FedeSeg <fedu.segura@gmail.com>
2021-06-11 23:30:31 -0300

    modelo de productos

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -3,28 +3,40 @@ const path = require('path');
 
 module.exports = {
     filename: path.resolve(__dirname, '../data/products.json'),
+
     readFile() {
-        // Leer nuestra informacion
         const productPath = this.filename;
         const planetsJson = fs.readFileSync(productPath, 'utf-8');
-        // Parsear la informacion
         return JSON.parse(planetsJson);
     },
+
     writeFile(newData) {
-        // Pasar la data a json
         const dataJson = JSON.stringify(newData, null, 2);
-        // Escribir el archivo
         fs.writeFileSync(this.filename, dataJson);
     },
+
     generateId(){
         const products = this.readFile();
         const lastProduct = products.pop();
         return lastProduct.id + 1
+    },
 
-    }
-    ,
+    findAll() {
+        const products = this.readFile();
+        return products
+    },
+
+    findbyPk(id) {
+        const products = this.readFile();
+        const productFound = products.find(product => product.id = id);
+        return productFound
+    },
+    
     create(product) {
         product.id = this.generateId();
         const products = this.readFile();
+        const productsUpdated = [...products, product];
+        this.writeFile(productsUpdated);
+        return product;
     }
 }
